fe84aeef1d32d1aa283073d72755f15c5158703e
hongsam14 <ghdtjdgus14@gmail.com>
2025-07-11 14:25:11 +0900

    feat(model): define syscall model

--- src/model.py
+++ src/model.py
@@ -1,3 +1,29 @@
 from pydantic import BaseModel
 from typing import Optional, List
 from datetime import datetime
+from uuid import UUID
+
+class SyscallObject(BaseModel):
+    """_summary_
+
+    Args:
+        BaseModel (_type_): 
+        BaseModel is a Pydantic model that provides data validation and serialization.
+    
+    Attributes:
+        syscall (str): Primary key for the graph object.
+        analysis_id (UUID): Unique identifier for the analysis.
+        parent (Optional[List[str]]): List of parent syscalls.
+        tactics (Optional[List[str]]): List of tactics associated with the syscall.
+        matched_ids (Optional[List[str]]): List of matched sigma rule IDs.
+        start_at (datetime): Timestamp when the object was created.
+        end_at (datetime): Timestamp when the object was last updated.
+    """
+    syscall: str
+    analysis_id: UUID
+    parent: Optional[List[str]] = None
+    tactics: Optional[List[str]] = None
+    matched_ids: Optional[List[str]] = None
+    start_at: datetime
+    end_at: datetime
+    
