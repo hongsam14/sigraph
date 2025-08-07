724f6debed1b70af1d94ac2ec9a2d95c7c7f96aa
hongsam14 <ghdtjdgus14@gmail.com>
2025-08-07 21:56:54 +0900

    chore(main): delete deprecated codes

--- src/db_client.py
+++ src/db_client.py
@@ -5,53 +5,53 @@ https://forum.opensearch.org/t/about-python-client-library-and-elasticsearch-dsl
 to support OpenSearch, you need to use the `elasticsearch` library 7.13 or earlier.
 """
 
-from elasticsearch_dsl import connections, Document, Keyword, Text, Date
-from model import SysLogObject
+# from elasticsearch_dsl import connections, Document, Keyword, Text, Date
+# from model import SysLogObject
 
-class SyslogDocument(Document):
-    """_summary_
-    SyslogDocument is a document model for storing syslog entries in OpenSearch.
-    """
-    analysis_id = Keyword()
-    span_id = Keyword()
-    trace_id = Keyword()
-    timestamp = Date()
-    raw_data = Text()
-
-
-    class Index:
-        name = 'syslog_index'
-        settings = {
-            "number_of_shards": 1,
-            "number_of_replicas": 0
-        }
+# class SyslogDocument(Document):
+#     """_summary_
+#     SyslogDocument is a document model for storing syslog entries in OpenSearch.
+#     """
+#     analysis_id = Keyword()
+#     span_id = Keyword()
+#     trace_id = Keyword()
+#     timestamp = Date()
+#     raw_data = Text()
 
 
-class DBClient:
-    """__summary__
-    DBClient is a client for interacting with opensearch for storing Syscall Objects.
-    """
-    __index_name: str
-    __syslog_doc: SyslogDocument
+#     class Index:
+#         name = 'syslog_index'
+#         settings = {
+#             "number_of_shards": 1,
+#             "number_of_replicas": 0
+#         }
 
 
-    def __init__(self, uri: str, index_name: str):
-        self.index_name = index_name
-        self.__syslog_doc = SyslogDocument()
+# class DBClient:
+#     """__summary__
+#     DBClient is a client for interacting with opensearch for storing Syscall Objects.
+#     """
+#     __index_name: str
+#     __syslog_doc: SyslogDocument
+
+
+#     def __init__(self, uri: str, index_name: str):
+#         self.index_name = index_name
+#         self.__syslog_doc = SyslogDocument()
         
-        print(f"Connecting to OpenSearch at {uri} with index {index_name}")
-        connections.create_connection(hosts=[f"http://{uri}:9200"])
+#         print(f"Connecting to OpenSearch at {uri} with index {index_name}")
+#         connections.create_connection(hosts=[f"http://{uri}:9200"])
         
-        # document initialization
-        self.__syslog_doc.init()
+#         # document initialization
+#         self.__syslog_doc.init()
 
-    def save_syslog_object(self, syslog_object: SysLogObject):
-        """Save a SysLogObject to OpenSearch."""
-        doc = SyslogDocument(
-            analysis_id=str(syslog_object.analysis_id),
-            span_id=syslog_object.span_id,
-            trace_id=syslog_object.trace_id,
-            timestamp=syslog_object.timestamp,
-            raw_data=syslog_object.raw_data
-        )
-        doc.save()
+#     def save_syslog_object(self, syslog_object: SysLogObject):
+#         """Save a SysLogObject to OpenSearch."""
+#         doc = SyslogDocument(
+#             analysis_id=str(syslog_object.analysis_id),
+#             span_id=syslog_object.span_id,
+#             trace_id=syslog_object.trace_id,
+#             timestamp=syslog_object.timestamp,
+#             raw_data=syslog_object.raw_data
+#         )
+#         doc.save()
--- src/main.py
+++ src/main.py
@@ -1,39 +1,48 @@
 import os
 from fastapi import FastAPI
 from dotenv import load_dotenv
-from model import SyscallNode, SysLogObject
-from graph_client import GraphClient
-from db_client import DBClient
+from loguru import logger
+from graph.graph_model import GraphNode
+from graph.graph_session import GraphSession
+# from db_client import DBClient
 
 # Load environment variables from .env file
 load_dotenv()
 
 # Initialize FastAPI application
 app = FastAPI()
-# Initialize the GraphClient with Neo4j connection details
-graph_client = GraphClient(
+
+logger.add("logs/app.log",
+           rotation="10 MB",
+           retention="7 days",
+           compression="zip",
+           level="INFO")
+
+# Initialize the GraphSession with Neo4j connection details
+graph_session = GraphSession(
+    logger,
     uri=os.getenv("NEO4J_URI", "localhost"),
     user=os.getenv("NEO4J_USER", "neo4j"),
     password=os.getenv("NEO4J_PASSWORD", "")
 )
 
-db_client = DBClient(
-    uri=os.getenv("OPENSEARCH_URI", "localhost"),
-    index_name=os.getenv("OPENSEARCH_INDEX", "syslog_index")
-)
+# db_client = DBClient(
+#     uri=os.getenv("OPENSEARCH_URI", "localhost"),
+#     index_name=os.getenv("OPENSEARCH_INDEX", "syslog_index")
+# )
 
 @app.post("/syscall")
-async def post_syscall(event: SyscallNode):
+async def post_syscall(event: GraphNode):
     try:
-        graph_client.upsert_syscall_object(event)
+        graph_session.upsert_system_provenance(event)
         return {"status": "ok"}
     except Exception as e:
         return {"status": "error", "message": str(e)}
     
-@app.post("/syslog")
-async def post_syslog(syslog_object: SysLogObject):
-    try:
-        db_client.save_syslog_object(syslog_object)
-        return {"status": "ok"}
-    except Exception as e:
-        return {"status": "error", "message": str(e)}
+# @app.post("/syslog")
+# async def post_syslog(syslog_object: SysLogObject):
+#     try:
+#         db_client.save_syslog_object(syslog_object)
+#         return {"status": "ok"}
+#     except Exception as e:
+#         return {"status": "error", "message": str(e)}
