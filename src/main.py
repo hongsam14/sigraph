e0ba3d665584bb33a94b6e90e1b8cd790538cf99
hongsam14 <ghdtjdgus14@gmail.com>
2025-08-08 15:29:56 +0900

    feat: reflect db session to main

--- src/main.py
+++ src/main.py
@@ -4,7 +4,8 @@ from dotenv import load_dotenv
 from loguru import logger
 from graph.graph_model import GraphNode
 from graph.graph_session import GraphSession
-# from db_client import DBClient
+from db.db_session import DBSession
+from db.db_model import SyslogModel
 
 # Load environment variables from .env file
 load_dotenv()
@@ -26,10 +27,11 @@ graph_session = GraphSession(
     password=os.getenv("NEO4J_PASSWORD", "")
 )
 
-# db_client = DBClient(
-#     uri=os.getenv("OPENSEARCH_URI", "localhost"),
-#     index_name=os.getenv("OPENSEARCH_INDEX", "syslog_index")
-# )
+db_session = DBSession(
+    logger,
+    uri=os.getenv("OPENSEARCH_URI", "localhost"),
+    index_name=os.getenv("OPENSEARCH_INDEX", "syslog_index")
+)
 
 @app.post("/syscall")
 async def post_syscall(event: GraphNode):
@@ -39,10 +41,10 @@ async def post_syscall(event: GraphNode):
     except Exception as e:
         return {"status": "error", "message": str(e)}
     
-# @app.post("/syslog")
-# async def post_syslog(syslog_object: SysLogObject):
-#     try:
-#         db_client.save_syslog_object(syslog_object)
-#         return {"status": "ok"}
-#     except Exception as e:
-#         return {"status": "error", "message": str(e)}
+@app.post("/syslog")
+async def post_syslog(syslog_object: SyslogModel):
+    try:
+        db_session.store_syslog_object(syslog_object)
+        return {"status": "ok"}
+    except Exception as e:
+        return {"status": "error", "message": str(e)}
