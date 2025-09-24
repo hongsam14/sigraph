4522858c8faa3f851626c74bce1f4c1f1630e3ed
hongsam14 <ghdtjdgus14@gmail.com>
2025-09-24 14:31:35 +0900

    chore: delete unused api from api service

--- src/app/backend/api.py
+++ src/app/backend/api.py
@@ -5,14 +5,13 @@ from app.backend.v1.api import DBAPI
 from app.backend.v1.api import AIAPI
 
 class BackendAPI:
-    # db_api: DBAPI
-    ai_api: AIAPI
-
+    db_api: DBAPI
+    # ai_api: AIAPI
     api_router = APIRouter(prefix="/api")
 
     def __init__(self, logger: Any, config: AppConfig):
         self.db_api = DBAPI(logger, config)
-        self.ai_api = AIAPI(logger, config)
+        # self.ai_api = AIAPI(logger, config)
         # Include the database API router
         self.api_router.include_router(self.db_api.api_router)
-        self.api_router.include_router(self.ai_api.api_router)
+        # self.api_router.include_router(self.ai_api.api_router)
